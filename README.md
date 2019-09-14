3eeed6e9ea3688de72d38db30113f469bc7f00cf
grieff <49982270+grieff@users.noreply.github.com>
2019-09-14 22:34:25 +0200

    Update .py, added sound

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pong-game
  Classic Pong Game created using turtle module in Python 3
+ Updated to add sound. Only works on Windows.
+ 
+ 
+ # What I Learned
+ 
+* How to use the turtle module to make a simple game
+* Implemented a scoring system
+* Implemented keyboard binding to move the paddles
+* Border checking and how to avoid paddles and ball going off screen
+* Implemented sound when the ball bounces on the paddle and at the top and the bottom of the screen (Windows only)
+ 
+ 
--- pong-game.py
+++ pong-game.py
@@ -3,6 +3,12 @@
 # Library used, installed on python
 import turtle
 
+# Sound, to be able to play the sounds
+# Windows ONLY
+import winsound
+
+
+
 wn = turtle.Screen()
 wn.title("Classic Pong Game")
 wn.bgcolor("black")
@@ -110,9 +116,13 @@ while True:
         ball.sety(290)
         ball.dy *= -1
 
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
+
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -140,12 +150,16 @@ while True:
             (ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
     # Paddle A / left
     if (ball.xcor() < -340 and ball.xcor() > -350) and \
             (ball.ycor() < paddle_a.ycor() + 40) and \
             (ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+
+        winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
 
     # Avoid Paddles going off screen
     if paddle_b.ycor() + 50 > 300:
